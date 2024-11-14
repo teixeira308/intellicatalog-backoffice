d007947c0a77cdcbdb2eec9a5583575858fdba0e
Guilherme Teixeira <teixeira308@hotmail.com>
2024-11-13 23:45:32 -0300

    fix size loading image

--- src/components/ModalCriarFotoLoja/styles.js
+++ src/components/ModalCriarFotoLoja/styles.js
@@ -160,8 +160,8 @@ export const Description = styled.p`
 `;
 
 export const LoadingImage = styled.img`
-   width: 60px; /* Largura das imagens */
-  height: 60px; /* Altura das imagens */
+   width: 60%; /* Largura das imagens */
+  height: 60%; /* Altura das imagens */
   background-color: #f10f10f10;  /* Cor de fundo leve */
   justify-content: center;
 `;
