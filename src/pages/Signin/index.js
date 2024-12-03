d3601f2098a84d84476a5db445ac0343352074e9
Guilherme Teixeira <teixeira308@hotmail.com>
2024-12-03 01:55:59 -0300

    link fix

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -58,7 +58,7 @@ const Signin = () => {
         <Button Text="Entrar" onClick={handleLogin} />
       <C.LabelSignup>
           <C.Strong>
-          <Link to="/esquecisenha">Esqueci minha senha</Link>
+          <C.Link onClick={() => { navigate("/esquecisenha");}}>Esqueceu sua senha? Clique aqui</C.Link>
           </C.Strong>
         </C.LabelSignup> 
       </C.Content>
