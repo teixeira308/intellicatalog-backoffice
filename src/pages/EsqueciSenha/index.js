84e4458366dcbf130e446584c74ca04eca007293
Guilherme Teixeira <teixeira308@hotmail.com>
2024-12-05 09:40:45 -0300

    fix

--- src/pages/EsqueciSenha/index.js
+++ src/pages/EsqueciSenha/index.js
@@ -1,41 +1,6 @@
-import { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import UserApi from "../../services/userApi";
 import * as C from "./styles";
 
 const EsqueciSenha = () => {
-
-    const { token } = useParams(); // Captura o token da URL
-    const navigate = useNavigate();
-    const [newPassword, setNewPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [message, setMessage] = useState("");
-    const [error, setError] = useState("");
-    const { updatePassword } = UserApi(); // Importa o serviço
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        if (newPassword !== confirmPassword) {
-            setError("As senhas não coincidem.");
-            return;
-        }
-
-        try {
-            const response = await updatePassword(token, newPassword);
-            setMessage(response.message);
-            setError("");
-
-            // Opcional: Redirecionar após sucesso
-            setTimeout(() => {
-                navigate("/login");
-            }, 3000);
-        } catch (err) {
-            setError(err);
-            setMessage("");
-        }
-    };
 
     return (
         <C.Container>
