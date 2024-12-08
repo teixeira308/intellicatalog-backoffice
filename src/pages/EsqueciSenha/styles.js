ef3343349ae9d7651848bf5efaba5a598349c42a
Guilherme Teixeira <teixeira308@hotmail.com>
2024-12-08 02:22:57 -0300

    fix css layout

--- src/pages/EsqueciSenha/styles.js
+++ src/pages/EsqueciSenha/styles.js
@@ -9,6 +9,19 @@ export const Container = styled.div`
   padding: 20px;
   background-color: #f4f4f4;
 `;
+export const Content = styled.div`
+  gap: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  box-shadow: 0 1px 2px #0003;
+  background-color: white;
+  max-width: 350px;
+  padding: 20px;
+  border-radius: 5px;
+`;
 
 export const Title = styled.h2`
   font-size: 2rem;
