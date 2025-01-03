f2fd50a769e0cc9505ff9e8d88b5a9649d69dff7
Guilherme Teixeira <teixeira308@hotmail.com>
2025-01-03 19:04:44 -0300

    fix css

--- src/pages/Catalogo/styles.js
+++ src/pages/Catalogo/styles.js
@@ -230,6 +230,7 @@ export const CategoriaList = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
+  width: 100px;
 `;
 export const CategoriaCardDragging = styled(Card)`
   background-color: #e0f7fa; /* Cor de destaque enquanto arrasta */
