42395963f528d4d1c8873b62d01e3ff57a24fc7c
Guilherme Teixeira <teixeira308@hotmail.com>
2025-01-10 00:51:27 -0300

    fix css

--- src/pages/Servicos/style.js
+++ src/pages/Servicos/style.js
@@ -44,7 +44,8 @@ export const Step = styled.div`
   border-radius: 8px;
   padding: 20px;
   margin: 10px 0;
-  width: 100%;
+ 
+    max-width: 800px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
