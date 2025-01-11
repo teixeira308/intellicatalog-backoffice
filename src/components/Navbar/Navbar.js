5a366cf48a3442914b018068dafe5ab78e08adaf
Guilherme Teixeira <teixeira308@hotmail.com>
2025-01-10 23:32:35 -0300

    fix menu responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,14 +3,18 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import * as C from "./styles";
 import logo from "../../assets/logo.png";
-import { Drawer, IconButton, List, ListItem, ListItemText, Divider } from "@mui/material";
+import { Drawer, IconButton, List, ListItem, ListItemText, Divider, useMediaQuery } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { useTheme } from "@mui/material/styles";
 
 const Navbar = () => {
   const { signout } = useAuth();
   const navigate = useNavigate();
   const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md")); // Detecta telas menores que "md" (960px)
 
   const toggleDrawer = (open) => () => {
     setDrawerOpen(open);
@@ -21,55 +25,73 @@ const Navbar = () => {
     setDrawerOpen(false); // Fecha o Drawer após a navegação
   };
 
+  // Menu de navegação (usado tanto no Drawer quanto no Desktop)
+  const renderMenuItems = () => (
+    <List>
+      <ListItem button onClick={() => handleNavigate("/home")}>
+        <ListItemText primary="Início" />
+      </ListItem>
+      <ListItem button onClick={() => handleNavigate("/minhaloja")}>
+        <ListItemText primary="Minha Loja" />
+      </ListItem>
+      <ListItem button onClick={() => handleNavigate("/catalogo")}>
+        <ListItemText primary="Catálogo" />
+      </ListItem>
+      <ListItem button onClick={() => handleNavigate("/servicos")}>
+        <ListItemText primary="Serviços" />
+      </ListItem>
+      <ListItem button onClick={() => handleNavigate("/agenda")}>
+        <ListItemText primary="Agenda" />
+      </ListItem>
+      <ListItem button onClick={() => handleNavigate("/tutorial")}>
+        <ListItemText primary="Tutorial" />
+      </ListItem>
+      <ListItem button onClick={() => handleNavigate("/demonstracao")}>
+        <ListItemText primary="Demonstração" />
+      </ListItem>
+      <ListItem button onClick={() => handleNavigate("/suporte")}>
+        <ListItemText primary="Suporte" />
+      </ListItem>
+      <Divider />
+      <ListItem button onClick={() => [signout(), navigate("/")]}>
+        <LogoutIcon style={{ marginRight: "10px" }} />
+        <ListItemText primary="Sair" />
+      </ListItem>
+    </List>
+  );
+
   return (
-    <>
-   
     <C.Navbar>
-    <C.Logo>
+      {/* Logo */}
+      <C.Logo>
         <C.LogoImage src={logo} alt="Logo" />&nbsp; Vitrine Smart
       </C.Logo>
 
-      {/* Botão para abrir o Drawer */}
-      <IconButton edge="start" color="inherit" onClick={toggleDrawer(true)}>
-        <MenuIcon />
-      </IconButton>
-
-      {/* Drawer do Material-UI */}
-      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
-        <List>
-          <ListItem button onClick={() => handleNavigate("/home")}>
-            <ListItemText primary="Início" />
-          </ListItem>
-          <ListItem button onClick={() => handleNavigate("/minhaloja")}>
-            <ListItemText primary="Minha Loja" />
-          </ListItem>
-          <ListItem button onClick={() => handleNavigate("/catalogo")}>
-            <ListItemText primary="Catálogo" />
-          </ListItem>
-          <ListItem button onClick={() => handleNavigate("/servicos")}>
-            <ListItemText primary="Serviços" />
-          </ListItem>
-          <ListItem button onClick={() => handleNavigate("/agenda")}>
-            <ListItemText primary="Agenda" />
-          </ListItem>
-          <ListItem button onClick={() => handleNavigate("/tutorial")}>
-            <ListItemText primary="Tutorial" />
-          </ListItem>
-          <ListItem button onClick={() => handleNavigate("/demonstracao")}>
-            <ListItemText primary="Demonstração" />
-          </ListItem>
-          <ListItem button onClick={() => handleNavigate("/suporte")}>
-            <ListItemText primary="Suporte" />
-          </ListItem>
-          <Divider />
-          <ListItem button onClick={() => [signout(), navigate("/")]}>
-            <LogoutIcon style={{ marginRight: "10px" }} />
-            <ListItemText primary="Sair" />
-          </ListItem>
-        </List>
-      </Drawer>
+      {isMobile ? (
+        // Menu Mobile com Drawer
+        <>
+          <IconButton edge="start" color="inherit" onClick={toggleDrawer(true)}>
+            <MenuIcon />
+          </IconButton>
+          <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+            {renderMenuItems()}
+          </Drawer>
+        </>
+      ) : (
+        // Menu Desktop
+        <C.NavLinks>
+          <C.Link onClick={() => handleNavigate("/home")}>Início</C.Link>
+          <C.Link onClick={() => handleNavigate("/minhaloja")}>Minha Loja</C.Link>
+          <C.Link onClick={() => handleNavigate("/catalogo")}>Catálogo</C.Link>
+          <C.Link onClick={() => handleNavigate("/servicos")}>Serviços</C.Link>
+          <C.Link onClick={() => handleNavigate("/agenda")}>Agenda</C.Link>
+          <C.Link onClick={() => handleNavigate("/tutorial")}>Tutorial</C.Link>
+          <C.Link onClick={() => handleNavigate("/demonstracao")}>Demonstração</C.Link>
+          <C.Link onClick={() => handleNavigate("/suporte")}>Suporte</C.Link>
+          <C.Link onClick={() => [signout(), navigate("/")]}>Sair</C.Link>
+        </C.NavLinks>
+      )}
     </C.Navbar>
-    </>
   );
 };
 
