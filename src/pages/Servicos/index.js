0c8556608542b738a71381792b926739398c867a
Guilherme Teixeira <teixeira308@hotmail.com>
2025-01-14 19:18:57 -0300

    fix

--- src/pages/Servicos/index.js
+++ src/pages/Servicos/index.js
@@ -72,7 +72,6 @@ const Servicos = () => {
   //EDITAR SERVIÇO
 
   const openEditarServicoModal = (servico) => {
-    console.log(servico)
     setSelectedServico(servico);
     setIsEditarServicoModalOpen(true);
   };
