7aca04a971aa02682de012759a438eab41b2366b
Guilherme Teixeira <teixeira308@hotmail.com>
2025-01-19 21:08:26 -0300

    fix

--- src/pages/Catalogo/index.js
+++ src/pages/Catalogo/index.js
@@ -433,7 +433,7 @@ const Catalogo = () => {
                           <C.ProdutoActions key={produto.id}>
                             <C.ProdutoItem>
                               <span>{produto.titulo}</span>
-                              <span><FaBoxOpen/>: {produto.estoque}</span>
+                              <span><FaBoxOpen/>: {produto.estoque || 0}</span>
                               <IconButton onClick={(event) => setMenuProdAnchor(event.currentTarget)}>
                                 <FaEllipsisV />
                               </IconButton>
