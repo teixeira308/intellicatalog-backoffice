dd91c6505fca17f00dc10d03c7c1bc0f9c5b3763
Guilherme Teixeira <teixeira308@hotmail.com>
2025-01-26 04:03:42 -0300

    fix

--- src/components/ModalEditarLojaConfig/EditarLojaConfigModal.js
+++ src/components/ModalEditarLojaConfig/EditarLojaConfigModal.js
@@ -158,6 +158,24 @@ const EditarLojaModal = ({ isOpen, onClose, loja, onEdit }) => {
                 style={{ marginRight: '10px' }} // Espaço entre o input e o div
               />
             </C.FormColumn>
+<br/>
+ <C.FormRow>
+            <div style={{
+             height: '30px',
+             backgroundColor: formData.cor_primaria, 
+             borderColor: formData.cor_botao_primaria, 
+             color: formData.cor_secundaria,
+             display: 'flex',                // Adiciona o display flex
+             justifyContent: 'center',       // Centraliza os botões horizontalmente
+             alignItems: 'center'            // Centraliza os botões verticalmente
+            }}>
+              <button onClick={(e) =>  e.preventDefault()} style={{ backgroundColor: formData.cor_botao_primaria, borderColor: formData.cor_botao_primaria, color: formData.cor_secundaria , marginRight: '10px'  }}>
+                Meu carrinho</button>
+
+              <button onClick={(e) =>  e.preventDefault()} style={{ backgroundColor: formData.cor_botao_secundaria, borderColor: formData.cor_botao_secundaria, color: formData.cor_secundaria }}>
+                Finalizar Pedido</button>
+            </div>
+          </C.FormRow>
             <C.FormColumn>
               <C.Label htmlFor="cor_botao_secundaria">Botão secundária</C.Label>
               <div style={{ width: '30px', height: '30px', backgroundColor: formData.cor_botao_secundaria, border: '1px solid #000', marginTop: '5px' }} />
