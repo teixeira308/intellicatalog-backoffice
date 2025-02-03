4c91baa6375b06fc0867aca4c3cebfeccf4e315f
Guilherme Teixeira <teixeira308@hotmail.com>
2025-02-03 10:30:44 -0300

    fix css

--- src/pages/Agenda/style.js
+++ src/pages/Agenda/style.js
@@ -2,8 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
- 
-  justify-content: center;
+  align-items: center;
   flex-direction: column;
   height: 100vh;
   padding-top: 20px;
