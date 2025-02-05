007fdf45806942b53adc85f8d4942676fe2808e8
Guilherme Teixeira <teixeira308@hotmail.com>
2025-02-05 17:44:55 -0300

    add mui

--- src/components/ModalEditarLojaConfig/EditarLojaConfigModal.js
+++ src/components/ModalEditarLojaConfig/EditarLojaConfigModal.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import * as C from "./styles";
 import lojaApi from "../../services/lojaApi";
 import { NumericFormat } from 'react-number-format';
+import { Modal, Box, Typography, TextField, Button, Checkbox, FormControlLabel } from "@mui/material";
 
 const EditarLojaModal = ({ isOpen, onClose, loja, onEdit }) => {
   const { updateLoja, getLojaConfig, updateLojaConfig } = lojaApi();
@@ -113,11 +114,26 @@ const EditarLojaModal = ({ isOpen, onClose, loja, onEdit }) => {
   if (!isOpen) return null;
 
   return (
-    <C.ModalOverlay>
-      <C.ModalContainer>
+    <Modal>
+      <Box sx={{
+        width: 400,
+        margin: 'auto',
+        padding: 3,
+        backgroundColor: 'white',
+        borderRadius: 2,
+        boxShadow: 24,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}>
         <C.ModalHeader>
           <h2>Configurações da Minha Loja</h2>
-          <C.CloseButton onClick={onClose}>&times;</C.CloseButton>
+
         </C.ModalHeader>
         <C.ModalForm onSubmit={handleSubmit}>
           Cores: <br /><br />
@@ -125,7 +141,7 @@ const EditarLojaModal = ({ isOpen, onClose, loja, onEdit }) => {
             <C.FormColumn style={{ flex: 1 }}>
               <C.Label htmlFor="cor_primaria">Tela</C.Label>
               <div style={{ width: '30px', height: '30px', backgroundColor: formData.cor_primaria, border: '1px solid #000', marginTop: '5px' }} />
-              <C.Input
+              {/*} <C.Input
                 type="color"
                 name="cor_primaria"
                 id="cor_primaria"
@@ -133,18 +149,40 @@ const EditarLojaModal = ({ isOpen, onClose, loja, onEdit }) => {
                 onChange={handleChange}
                 required
                 style={{ marginRight: '10px' }} // Espaço entre o input e o div
+              /> {*/}
+
+              <TextField
+                type="color"
+                name={cor_primaria}
+                id={cor_primaria}
+                value={formData.cor_primaria}
+                onChange={handleChange}
+                fullWidth
+                sx={{ marginRight: '10px' }}
               />
+
+
             </C.FormColumn>
             <C.FormColumn>
               <C.Label htmlFor="cor_secundaria">Fonte</C.Label>
               <div style={{ width: '30px', height: '30px', backgroundColor: formData.cor_secundaria, border: '1px solid #000', marginTop: '5px' }} />
-              <C.Input
+              {/*} <C.Input
                 type="color"
                 name="cor_secundaria"
                 id="cor_secundaria"
                 value={formData.cor_secundaria}
                 onChange={handleChange}
                 style={{ marginRight: '10px' }} // Espaço entre o input e o div
+              /> {*/}
+
+              <TextField
+                type="color"
+                name={cor_secundaria}
+                id={cor_secundaria}
+                value={formData.cor_secundaria}
+                onChange={handleChange}
+                fullWidth
+                sx={{ marginRight: '10px' }}
               />
             </C.FormColumn>
             <C.FormColumn>
@@ -195,20 +233,34 @@ const EditarLojaModal = ({ isOpen, onClose, loja, onEdit }) => {
           </C.FormRow>
           <C.FormRow>
             <C.FormColumn>
-              <C.Label htmlFor="usa_logo_fundo">Usar logo como fundo da página{"  "}
+              {/*} <C.Label htmlFor="usa_logo_fundo">Usar logo como fundo da página{"  "}
                 <C.Input
                   type="checkbox"
                   name="usa_logo_fundo"
                   id="usa_logo_fundo"
                   checked={formData.usa_logo_fundo}
                   onChange={handleChangeCheckBox}
-                /></C.Label>
+                />
+                
+                </C.Label>
+ {*/}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    name="usa_logo_fundo"
+                    id="usa_logo_fundo"
+                    checked={formData.usa_logo_fundo}
+                    onChange={handleChangeCheckBox}
+                  />
+                }
+                label="Usar logo de fundo"
+              />
             </C.FormColumn>
           </C.FormRow>
 
           <C.FormRow>
             <C.FormColumn>
-              <C.Label htmlFor="usa_estoque">Usa estoque{"  "}
+              {/*} <C.Label htmlFor="usa_estoque">Usa estoque{"  "}
                 <C.Input
                   type="checkbox"
                   name="usa_estoque"
@@ -216,13 +268,25 @@ const EditarLojaModal = ({ isOpen, onClose, loja, onEdit }) => {
                   checked={formData.usa_estoque}  // Utilizando `checked` com o valor booleano
                   onChange={handleChangeCheckBox}  // Atualiza o estado ao marcar/desmarcar
                 /></C.Label>
+    {*/}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    name="usa_estoque"
+                    id="usa_estoque"
+                    checked={formData.usa_estoque}
+                    onChange={handleChangeCheckBox}
+                  />
+                }
+                label="Usa estoque"
+              />
             </C.FormColumn>
           </C.FormRow>
 
           <C.FormRow>
             <C.FormColumn>
-              <C.Label htmlFor="taxa_entrega">Taxa de entrega</C.Label>
-          
+              {/*}  <C.Label htmlFor="taxa_entrega">Taxa de entrega</C.Label>
+
               <NumericFormat
                 name="taxa_entrega"
                 id="taxa_entrega"
@@ -245,51 +309,85 @@ const EditarLojaModal = ({ isOpen, onClose, loja, onEdit }) => {
                   borderRadius: '4px',
                   fontSize: '1rem',
                   width: '100%',
+                }}/>
+{*/}
+              <NumericFormat
+                customInput={TextField}
+                label="Taxa de entrega"
+                name="taxa_entrega"
+                id="taxa_entrega"
+                value={formData.taxa_entrega}
+                onValueChange={(values) => {
+                  const { value } = values; // Obtém o valor numérico
+                  setFormData((prevFormData) => ({
+                    ...prevFormData,
+                    price: value, // Atualiza o valor no formData
+                  }));
                 }}
+                thousandSeparator="."
+                decimalSeparator=","
+                decimalScale={2}
+                fixedDecimalScale
+                prefix="R$ "
+                placeholder="R$ 0,00"
+                fullWidth
+                sx={{ mb: 2 }} // Espaçamento inferior
               />
+
             </C.FormColumn>
             <C.FormColumn>
-              <C.Label htmlFor="numero_whatsapp">Whatsapp</C.Label>
-              <C.Input
-                type="text"
+
+
+              <TextField
+                label="Whatsapp"
                 name="numero_whatsapp"
                 id="numero_whatsapp"
                 value={formData.numero_whatsapp}
                 onChange={handleChange}
+                fullWidth
+                sx={{ mb: 2 }}
               />
             </C.FormColumn>
           </C.FormRow>
 
           <C.FormRow>
             <C.FormColumn>
-              <C.Label htmlFor="instagram">Instagram</C.Label>
-              <C.Input
-                type="text"
+
+              <TextField
+                label="Instagram"
                 name="instagram"
                 id="instagram"
                 value={formData.instagram}
                 onChange={handleChange}
+                fullWidth
+                sx={{ mb: 2 }}
               />
+
             </C.FormColumn>
           </C.FormRow>
 
           <C.FormRow>
             <C.FormColumn>
-              <C.Label htmlFor="facebook">Facebook</C.Label>
-              <C.Input
-                type="text"
+
+              <TextField
+                label="Facebook"
                 name="facebook"
                 id="facebook"
                 value={formData.facebook}
                 onChange={handleChange}
+                fullWidth
+                sx={{ mb: 2 }}
               />
             </C.FormColumn>
           </C.FormRow>
 
-          <C.Button type="submit">Salvar</C.Button>
+          <Box display="flex" justifyContent="flex-end">
+            <Button onClick={onClose} variant="outlined" color="error" sx={{ mr: 2 }}>Cancelar</Button>
+            <Button type="submit" color="success" variant="contained">Salvar</Button>
+          </Box>
         </C.ModalForm>
-      </C.ModalContainer>
-    </C.ModalOverlay>
+      </Box >
+    </Modal>
   );
 };
 
