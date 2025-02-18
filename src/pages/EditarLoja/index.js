7b55bc0405be92bccbe5bd408f9ff52f51e8c352
Guilherme Teixeira <teixeira308@hotmail.com>
2025-02-18 16:14:39 -0300

    fix

--- src/pages/EditarLoja/index.js
+++ src/pages/EditarLoja/index.js
@@ -20,7 +20,7 @@ const EditarLoja = () => {
   const { identificadorexterno } = useParams();
   const navigate = useNavigate();
   const { getStoreByIdentificador, updateLoja } = LojaApi();
-  const [store, setStore] = useState({
+  const [formData, setFormData] = useState({
     namestore: "",
     opening_hours: "",
     closing_hours: "",
@@ -59,17 +59,15 @@ const EditarLoja = () => {
         setLoading(false);
       }
     };
-    
-    if (identificadorexterno) {
-      fetchStore();
-    }
-  }, [identificadorexterno, getStoreByIdentificador]);
+    fetchStore();
+  }, [identificadorexterno]);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setStore((prevStore) => ({ ...prevStore, [name]: value }));
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
-
   const handlePriceChange = (e) => {
     let value = e.target.value.replace(/[^0-9.]/g, '');
     const parts = value.split('.');
@@ -84,7 +82,7 @@ const EditarLoja = () => {
 
   const handleSubmit = async () => {
     try {
-      const filteredData = filterFormData(store);
+      const filteredData = filterFormData(FormData);
       await updateLoja(identificadorexterno, filteredData);
       alert("Loja atualizada com sucesso!");
       navigate(-1);
@@ -110,18 +108,17 @@ const EditarLoja = () => {
         </Box>
 
         <Stack spacing={2}>
-          <TextField label="Nome da Loja" name="namestore" fullWidth value={store.namestore ?? ""} onChange={handleChange} required />
-          <TextField label="Horário de Abertura" name="opening_hours" fullWidth value={store.opening_hours ?? ""} onChange={handleChange} />
-          <TextField label="Horário de Fechamento" name="closing_hours" fullWidth value={store.closing_hours ?? ""} onChange={handleChange} />
-          <TextField label="Identificador Externo" name="identificadorexterno" fullWidth value={store.identificadorexterno ?? ""} onChange={handleChange} required />
-          <TextField label="Endereço" name="address" fullWidth value={store.address ?? ""} onChange={handleChange} />
-          <TextField label="Telefone" name="phone" fullWidth value={store.phone ?? ""} onChange={handleChange} required />
-          <TextField label="E-mail" name="email" fullWidth value={store.email ?? ""} onChange={handleChange} />
-          <TextField label="Taxa de Entrega" name="delivery_fee" fullWidth type="text" value={store.delivery_fee ?? ""} onChange={handlePriceChange} />
+          <TextField label="Nome da Loja" name="namestore" fullWidth value={FormData.namestore ?? ""} onChange={handleChange} required />
+          <TextField label="Horário de Abertura" name="opening_hours" fullWidth value={FormData.opening_hours ?? ""} onChange={handleChange} />
+          <TextField label="Horário de Fechamento" name="closing_hours" fullWidth value={FormData.closing_hours ?? ""} onChange={handleChange} />
+          <TextField label="Identificador Externo" name="identificadorexterno" fullWidth value={FormData.identificadorexterno ?? ""} onChange={handleChange} required />
+          <TextField label="Endereço" name="address" fullWidth value={FormData.address ?? ""} onChange={handleChange} />
+          <TextField label="Telefone" name="phone" fullWidth value={FormData.phone ?? ""} onChange={handleChange} required />
+          <TextField label="E-mail" name="email" fullWidth value={FormData.email ?? ""} onChange={handleChange} />
+          <TextField label="Taxa de Entrega" name="delivery_fee" fullWidth type="text" value={FormData.delivery_fee ?? ""} onChange={handlePriceChange} />
         </Stack>
         
-        <Box display="flex" justifyContent="flex-end" sx={{ mt: 2 }}>
-          <Button onClick={() => navigate(-1)} variant="outlined" color="error" sx={{ mr: 2 }}>Cancelar</Button>
+        <Box display="flex" justifyContent="center" sx={{ mt: 2 }}>
           <Button variant="contained" color="success" onClick={handleSubmit}>Salvar</Button>
         </Box>
       </Container>
