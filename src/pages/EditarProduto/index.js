c870488fc85fcc264d9ff076137ef3c57b0c5b43
Guilherme Teixeira <teixeira308@hotmail.com>
2025-02-21 18:22:34 -0300

    fix product api import

--- src/pages/EditarProduto/index.js
+++ src/pages/EditarProduto/index.js
@@ -14,6 +14,7 @@ import {
   Select,
 } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ProductApi from "../../services/productApi";
 
 const EditarProduto = () => {
   const { id } = useParams();
